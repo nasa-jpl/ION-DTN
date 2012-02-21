ecb7456f909d54ac2164357aa70b9e865e453097
jswaro <js290006>
2012-02-21 17:27:01 -0500

    Updated version number to 3.0.  Created release notes for issues merged so far.

--- ici/include/ion.h
+++ ici/include/ion.h
@@ -22,8 +22,8 @@ extern "C" {
 
 /* Allow the compile option -D to override this in the future */
 #ifndef IONVERSIONNUMBER
-/* As of 2012-1-23 the sourceforge version number is this: */
-#define IONVERSIONNUMBER "2.5.3"
+/* As of 2012-16-23 the sourceforge version number is this: */
+#define IONVERSIONNUMBER "3.0.0"
 #endif
 
 /* Allow the compile option -D to override this in the future */
