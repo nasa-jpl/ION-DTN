5439979d027de3b7f480e2bc64494f11d911631e
jswaro <js290006>
2013-03-13 22:30:42 -0400

    Updated version number to 3.1.2.  Added release notes for all issues merged so far.

--- ici/include/ion.h
+++ ici/include/ion.h
@@ -23,8 +23,8 @@ extern "C" {
 
 /* Allow the compile option -D to override this in the future */
 #ifndef IONVERSIONNUMBER
-/* As of 2012-12-13 the sourceforge version number is this: */
-#define IONVERSIONNUMBER "ION OPEN SOURCE 3.1.1"
+/* As of 2013-03-13 the sourceforge version number is this: */
+#define IONVERSIONNUMBER "ION OPEN SOURCE 3.1.2"
 #endif
 
 /* Allow the compile option -D to override this in the future */
