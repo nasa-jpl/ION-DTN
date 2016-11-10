fd9c8fe3190cda3ac061299ee8400e704af01f5c
Scott Burleigh <scott.c.burleigh@jpl.nasa.gov>
2016-11-10 12:00:11 -0800

    First round of fixes for loopback tests.

--- tests/library/ionstop.c
+++ tests/library/ionstop.c
@@ -10,27 +10,21 @@
 void ionstop()
 {
 	int pid;
-	int status;
 
 	pid = pseudoshell("bpadmin .");
 	fail_unless(pid != ERROR);
-	fail_unless(-1 != waitpid(pid, &status, 0),
-		"Failed to wait for bpadmin to stop.");
+	sleep(1);
 
 	pid = pseudoshell("ltpadmin .");
 	fail_unless(pid != ERROR);
-	fail_unless(-1 != waitpid(pid, &status, 0),
-		"Failed to wait for ltpadmin to stop.");
+	sleep(1);
 
 	pid = pseudoshell("ionadmin .");
 	fail_unless(pid != ERROR);
-	fail_unless(-1 != waitpid(pid, &status, 0), 
-		"Failed to wait for ionadmin to stop.");
+	sleep(1);
 
 #if ! defined (VXWORKS) && ! defined (RTEMS)
 	pid = pseudoshell("killm");
 	fail_unless(pid != ERROR);
-	fail_unless(-1 != waitpid(pid, &status, 0),
-		"Failed to wait for killm to finish");
 #endif
 }
