b1b8c2f16447832debd1a07bc0f4dc950d3c393c
Patricia Lindner <lindnerp@ohio.edu>
2017-07-19 10:39:26 -0400

    Fix compile error on Mac OS X

--- ici/sdr/sdrmgt.c
+++ ici/sdr/sdrmgt.c
@@ -208,7 +208,7 @@ void	sdr_stage(Sdr sdrv, char *into, Object from, size_t length)
 	}
 
 	to = addr + length;
-	if (addr < 0 || to < addr || (to == addr && length != 0)
+	if (to < addr || (to == addr && length != 0)
 			|| to > sdr->dsSize)
 	{
 		putErrmsg(_violationMsg(), "stage");
