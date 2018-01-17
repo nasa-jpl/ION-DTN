7678b2038b42e235b840f4d05111767004db8e2e
Scott Burleigh <scott.c.burleigh@jpl.nasa.gov>
2018-01-16 16:44:41 -0800

    Add BSS, BSSP, and AMS to standard build for RTEMS.

--- arch-rtems/mysymtab.c
+++ arch-rtems/mysymtab.c
@@ -36,6 +36,18 @@ extern int	ipnadminep(int, int, int, int, int, int, int, int, int, int);
 extern int	lgagent(int, int, int, int, int, int, int, int, int, int);
 extern int	bpsource(int, int, int, int, int, int, int, int, int, int);
 extern int	bpsink(int, int, int, int, int, int, int, int, int, int);
+extern int	bsspclock(int, int, int, int, int, int, int, int, int, int);
+extern int	bsspadmin(int, int, int, int, int, int, int, int, int, int);
+extern int	udpbsi(int, int, int, int, int, int, int, int, int, int);
+extern int	udpbso(int, int, int, int, int, int, int, int, int, int);
+extern int	tcpbsi(int, int, int, int, int, int, int, int, int, int);
+extern int	tcpbso(int, int, int, int, int, int, int, int, int, int);
+extern int	ramsgate(int, int, int, int, int, int, int, int, int, int);
+extern int	amsshell(int, int, int, int, int, int, int, int, int, int);
+extern int	amslog(int, int, int, int, int, int, int, int, int, int);
+extern int	amslogprt(int, int, int, int, int, int, int, int, int, int);
+extern int	amsmib(int, int, int, int, int, int, int, int, int, int);
+extern int	amsstop(int, int, int, int, int, int, int, int, int, int);
 #ifndef NASA_PROTECTED_FLIGHT_CODE
 extern int	cfdpadmin(int, int, int, int, int, int, int, int, int, int);
 extern int	cfdpclock(int, int, int, int, int, int, int, int, int, int);
@@ -84,7 +96,19 @@ FUNCPTR	sm_FindFunction(char *name, int *priority, int *stackSize)
 		{ "ipnadminep",	(FUNCPTR) ipnadminep,	ICI_PRIORITY,	24576 },
 		{ "lgagent",	(FUNCPTR) lgagent,	ICI_PRIORITY,	24576 },
 		{ "bpsource",	(FUNCPTR) bpsource,	ICI_PRIORITY,	4096  },
-		{ "bpsink",	(FUNCPTR) bpsink,	ICI_PRIORITY,	4096  }
+		{ "bpsink",	(FUNCPTR) bpsink,	ICI_PRIORITY,	4096  },
+		{ "bsspclock",	(FUNCPTR) bsspclock,	ICI_PRIORITY,	25576 },
+		{ "bsspadmin",	(FUNCPTR) bsspadmin,	ICI_PRIORITY,	25576 },
+		{ "udpbsi",	(FUNCPTR) udpbsi,	ICI_PRIORITY,	25576 },
+		{ "udpbso",	(FUNCPTR) udpbso,	ICI_PRIORITY,	25576 },
+		{ "tcpbsi",	(FUNCPTR) tcpbsi,	ICI_PRIORITY,	25576 },
+		{ "tcpbso",	(FUNCPTR) tcpbso,	ICI_PRIORITY,	25576 },
+		{ "ramsgate",	(FUNCPTR) ramsgate,	ICI_PRIORITY,	25576 },
+		{ "amsshell",	(FUNCPTR) amsshell,	ICI_PRIORITY,	25576 },
+		{ "amslog",	(FUNCPTR) amslog,	ICI_PRIORITY,	25576 },
+		{ "amslogprt",	(FUNCPTR) amslogprt,	ICI_PRIORITY,	25576 },
+		{ "amsmib",	(FUNCPTR) amsmib,	ICI_PRIORITY,	25576 },
+		{ "amsstop",	(FUNCPTR) amsstop,	ICI_PRIORITY,	25576 }
 #ifndef NASA_PROTECTED_FLIGHT_CODE
 		,{ "cfdpadmin",	(FUNCPTR) cfdpadmin,	ICI_PRIORITY,	24576 },
 		{ "cfdpclock",	(FUNCPTR) cfdpclock,	ICI_PRIORITY,	24576 },
