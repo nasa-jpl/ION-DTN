aa1982773b27800a9b92c0e1af3c51d6e207c597
Sky DeBaun <sky.u.debaun@jpl.nasa.gov>
2022-09-22 21:00:05 +0000

    comment regarding 16 bit limitation to SANA range

--- ams/library/amscommon.h
+++ ams/library/amscommon.h
@@ -12,9 +12,9 @@
 	Jet Propulsion Laboratory 2022
 	
 	Modifications address the following:
-	1.) Allow for SANA node numbers (see MAX_CONTIN_NBR directive)
-	Note: this is currently constrained by the size
-	of a Short (for MacOS) 
+	1.) Allow for SANA range of node numbers (see MAX_CONTIN_NBR directive)
+		Note: this is currently constrained by the 16 bit field width in AMS' 
+		constructMessage() header array
 
 	2.) New associated function definitions for: 
 		getContinuaByNbr() and getMsgSpaceByNbr()
