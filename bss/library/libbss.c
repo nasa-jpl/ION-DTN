c0c58a45c6c691d0c83d193740a9887cee99ba2e
Jay Gao <Jay.L.Gao@jpl.nasa.gov>
2023-02-10 23:42:07 -0800

    fix typo

--- bss/library/libbss.c
+++ bss/library/libbss.c
@@ -116,7 +116,7 @@ int	bssOpen(char* bssName, char* path)
 		PUTS("An active playback session was detected.  If you \
 wish to initiate a new one, please first close the active playback session.");
 		fflush(stdout);
-		/* do nothing, keep existing existing database opened */
+		/* do nothing, keep existing database opened */
 		return -2;
 	}
 	
