e5f343ca1ec60044ee980fe07c4077e5998e44ec
iondev33 <iondev33@users.noreply.github.com>
2023-12-13 01:43:24 +0000

    deploy: 9f51ded4369d1652a5536e4d64c08d0cab4a944d

--- convert-pod-to-md.sh
+++ convert-pod-to-md.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e  # Exit immediately if a command exits with a non-zero status.
+
 # Predefined list of directory pairs
 # Format: "search_directory link_directory"
 DIR_PAIRS=(
@@ -25,23 +27,24 @@ convert_pod_to_md() {
   echo "Processing .pod files in $SEARCH_DIR to store in $LINK_DIR..."
 
   # Create the link directory if it doesn't exist
-  mkdir -p "$LINK_DIR"
+  mkdir -p "$LINK_DIR" || { echo "Failed to create directory $LINK_DIR"; exit 1; }
 
   # Convert .pod files to Markdown and store them in LINK_DIR
   find "$SEARCH_DIR" -name '*.pod' -exec sh -c '
     for file do
       # Convert to Markdown and store in LINK_DIR
-      pod2markdown "$file" > "'$LINK_DIR'/$(basename "${file%.pod}.md")"
+      pod2markdown "$file" > "'$LINK_DIR'/$(basename "${file%.pod}.md")" || { echo "Failed to convert file $file"; exit 1; }
     done
-  ' sh {} +
+  ' sh {} + || { echo "Conversion failed in directory $SEARCH_DIR"; exit 1; }
 
   # Create an index.md file with links to all Markdown files except itself
   echo "Creating index.md in $LINK_DIR..."
-  echo "# Index of Man Pages" > "$LINK_DIR/index.md"
+  echo "# Index of Man Pages" > "$LINK_DIR/index.md" || { echo "Failed to create index.md in $LINK_DIR"; exit 1; }
+  
   for mdfile in "$LINK_DIR"/*.md; do
     filename=$(basename "$mdfile")
     if [ "$filename" != "index.md" ]; then
-      echo "- [${filename%.md}]($filename)" >> "$LINK_DIR/index.md"
+      echo "- [${filename%.md}]($filename)" >> "$LINK_DIR/index.md" || { echo "Failed to write to index.md in $LINK_DIR"; exit 1; }
     fi
   done
 }
@@ -50,5 +53,5 @@ convert_pod_to_md() {
 for pair in "${DIR_PAIRS[@]}"; do
   # Split the pair into SEARCH_DIR and LINK_DIR
   read -r SEARCH_DIR LINK_DIR <<< "$pair"
-  convert_pod_to_md "$SEARCH_DIR" "$LINK_DIR"
+  convert_pod_to_md "$SEARCH_DIR" "$LINK_DIR" || { echo "Error processing $SEARCH_DIR and $LINK_DIR"; exit 1; }
 done
