7cae1a8dd270cd5c4f1744030ecf6ad696b2b981
sdebaun <sky.u.debaun@jpl.nasa.gov>
2024-08-06 19:31:46 +0000

    modified bpsec python tests suite to not call .ionstop script at shutdown, killm is sufficient

--- tests/bpsec/python_tests/tests/testUtils.py
+++ tests/bpsec/python_tests/tests/testUtils.py
@@ -813,8 +813,8 @@ def stop_and_clean():
     for i in d:
         if os.DirEntry.is_dir(i) and "ipn.ltp" in i.name:
             os.chdir(i.name)
-            subprocess.run("./ionstop &", shell=True, stdout=verbose)
-            os.chdir("..")
+            #subprocess.run("./ionstop &", shell=True, stdout=verbose)
+            #os.chdir("..")
     
     time.sleep(TIME_TESTFINISH)
     
