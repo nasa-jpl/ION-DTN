c8f95b1d0b25c92d99eb402ef305ec78836e982a
skydebaun <sdebaun@jpl.nasa.gov>
2024-09-27 01:34:39 +0000

    Additional update to resolve failing key copy operation

--- ici/crypto/MBEDTLS_SUITES/secrypt.c
+++ ici/crypto/MBEDTLS_SUITES/secrypt.c
@@ -199,12 +199,12 @@ int crypt_and_hash_buffer(
 {    
     int i; //iterator
     int status = -1;
-    unsigned n; //used for key
+    //unsigned n; //used for key
     int exit_code = MBEDTLS_EXIT_FAILURE; //default to failure
     size_t keylen=0, ilen=0, olen=0;
     size_t input_buffer_size = *input_length;
     
-    char *p = NULL;
+    //char *p = NULL;
     unsigned char *output_buffer = NULL;
     size_t output_length = 0; //return 0 on failure
     
@@ -295,7 +295,7 @@ int crypt_and_hash_buffer(
     }
 
     /*
-     * Read the secret key from file (or direct user input if no key found)
+     * Read the secret key from file
      */
     if ((fkey = fopen(my_key, "rb")) != NULL) 
     {
@@ -303,7 +303,8 @@ int crypt_and_hash_buffer(
         fclose(fkey);
     } 
     else 
-    {
+     {
+        /*
         printf("WARNING: using literal value as key (no key file found)!\n");
         if (memcmp(my_key, "hex:", 4) == 0) {
             p = &my_key[4];
@@ -315,7 +316,7 @@ int crypt_and_hash_buffer(
                 key[keylen++] = (unsigned char) n;
                 p += 2;
             }
-        } else 
+        } else  */
         {
             keylen = strlen(my_key);
 
