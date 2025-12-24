707356d4f152838d54bf15fc26296696e4f0ac4a
Scott Burleigh <sburleig@earthlink.net>
2025-12-24 01:01:38 -0800

    dtka.h: Comment a description of DTKA

--- tc/dtka/dtka.h
+++ tc/dtka/dtka.h
@@ -17,6 +17,19 @@ extern "C"
 {
 #endif
 
+/*
+ * DTKA is a TC (Trusted Collective) application.  Every TC application is
+ * identified by the multicast group number at which the application's TC client
+ * daemon listens for "blocks" published by the TC aggregate authority.
+ * The DTKA application is identified by the block multicast group number 203.
+ *
+ * Additionally, every TC application is configured to send new information to
+ * the TC aggregate authority in bundles whose destination cites the multicast
+ * group number at which the authority's reception daemon listens for "records"
+ * published by the application.  The records multicast group number for DTKA is
+ * 201.
+ */
+
 #define DTKA_DECLARE 201
 #define DTKA_ANNOUNCE 203
 
