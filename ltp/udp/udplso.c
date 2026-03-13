63a77d7675d4e7ffe68bd1b07a60b1dd19284969
sdebaun <sky.u.debaun@jpl.nasa.gov>
2026-03-13 03:16:40 +0000

    Reduced UDPLSA_BUFSZ size to account for Solaris-related constraints(smaller kernel buffer + owltsim bursts during regression test) after inclusion of the UDPLSA_BUFSZ

--- ltp/udp/udplso.c
+++ ltp/udp/udplso.c
@@ -475,7 +475,13 @@ int	main(int argc, char *argv[])
 
 	tb.lastRefillTime = getUsecTimestamp();
 	tb.tokens = 0;
-	tb.bucketSize = UDPLSA_BUFSZ * 2;
+
+	/*
+	 * UDPLSA_BUFSZ can be increased for improved LTP performance
+	 * on hosts with increased kernel buffer sizes. On Linux, x2
+	 * can be considered safe with the default kernel buffer size.
+	 */
+	tb.bucketSize = UDPLSA_BUFSZ;
 	tb.remoteEngineId = remoteEngineId;
 	tb.neighbor = NULL;
 
