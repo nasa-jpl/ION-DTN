8291e55759337b5f0b3161104db21b1bb70eb84e
250MHz <95242017+250MHz@users.noreply.github.com>
2026-05-22 17:15:53 -0700

    Introduce SCRIPT_DIR to runtests and pretest-script
    Logic to get the directory that runtests is in is repeated, so
    centralize it. Using $0 instead of ${BASH_SOURCE[0]} due to our interest
    in eventually removing Bash as a dependency. We use "." instead of
    "source" for pretest-script for the same reason.

--- tests/pretest-script.sh
+++ tests/pretest-script.sh
@@ -1,4 +1,5 @@
-export IONDIR="$PWD/.."
+# SCRIPT_DIR comes from runtests
+export IONDIR="$(cd "$SCRIPT_DIR/.." && pwd)"
 export PATH=$PATH:$IONDIR
 export LD_LIBRARY_PATH=$IONDIR/.libs:$LD_LIBRARY_PATH
 export CONFIGSROOT="$IONDIR/configs"
