85c6bed9fbdfe543878273b8b731aad206e3feba
250MHz <95242017+250MHz@users.noreply.github.com>
2026-06-12 17:45:06 -0700

    ci: Build with silent rules disabled
    Ensure that we build with verbose rules. Will matter once we enable
    silent rules by default.

--- .github/scripts/compile-and-buildcheck.sh
+++ .github/scripts/compile-and-buildcheck.sh
@@ -20,7 +20,9 @@ autoreconf -fi
 echo "Running configure..."
 # EXTRA_CONFIGURE_FLAGS allows workflows to pass OS-specific flags (like CFLAGS for ARC)
 # shellcheck disable=SC2086
-./configure --enable-crypto-mbedtls --enable-bpsec-debugging $EXTRA_CONFIGURE_FLAGS
+./configure --enable-crypto-mbedtls --enable-bpsec-debugging \
+    --disable-silent-rules \
+    $EXTRA_CONFIGURE_FLAGS
 
 echo "Running ${MAKE_CMD} all..."
 $MAKE_CMD -j"$(nproc)" all
